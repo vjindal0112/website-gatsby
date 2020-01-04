54dc3490581fd29c4cd73c9827f1a0716bee3d70
Varun Jindal <vjindal0112@gmail.com>
2020-01-03 19:23:08 -0500

    fixed icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,13 +57,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Varun Personal Website`,
+        short_name: `Varun`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/v-icon.ico`,
       },
     },
     `gatsby-plugin-offline`,

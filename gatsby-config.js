c71a843307380a501621a1aa67353cccacabbf0c
Varun Jindal <vjindal0112@gmail.com>
2020-01-04 20:47:20 -0500

    added analytics id

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,7 +50,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-83361158-1`,
       },
     },
     `gatsby-plugin-feed`,
